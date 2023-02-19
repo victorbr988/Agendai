1c13f3888d7a077358d3b9faf5739c9827215bc3
Victor Gabriel <gabrielvghs666@gmail.com>
2023-02-19 16:21:28 -0300

    feat: create page signup

---
+++ src/pages/Signup/index.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { Header } from "../../components/Header";
+
+export function Signup() {
+  return (
+    <View>
+      <Header />
+      <Text>Hello Signup</Text>
+    </View>
+  )
+}
