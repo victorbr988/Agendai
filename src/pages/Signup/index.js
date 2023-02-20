73ed786ffd137bcec337612b9509f40d7509d7bd
Victor Gabriel <gabrielvghs666@gmail.com>
2023-02-20 00:48:01 -0300

    feat: create page to signup

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,11 +1,25 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { useForm } from "../../components/Form";
 import { Header } from "../../components/Header";
+import { useLoginStore } from "../../zustand";
 
-export function Signup() {
+export function Signup({ navigation }) {
+  const FormClient = useForm('Signup_user_client')
+  const FormBarber = useForm('Signup_user_barber')
+  const typeUser = useLoginStore(state => state.typeUser)
+
+  const TypeForms = {
+    "Cliente": FormClient,
+    "Barbearia": FormBarber
+  }
+
+  const FormByUser = TypeForms[typeUser]
   return (
     <View>
       <Header />
-      <Text>Hello Signup</Text>
+      <View style={{marginLeft: 20, marginRight: 20}}>
+        <FormByUser navigation={navigation} />
+      </View>
     </View>
   )
 }
